1020a5c1f0ba3b5899811db2e370534d5f5dde10
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-11-05 15:40:07 -0300

    feat: create user via SignUp page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -14,6 +14,7 @@ import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
+import api from '../../services/api';
 import logoImg from '../../assets/logo.png';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -46,6 +47,14 @@ const SignUp: React.FC = () => {
       });
 
       await shema.validate(data, { abortEarly: false });
+
+      await api.post('/users', data);
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'Você já pode fazer login no aplicativo.',
+      );
+
+      navigation.goBack();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
