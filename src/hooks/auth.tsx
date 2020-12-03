594bd5da4a6128c18ef34799ec609c699adc650d
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-12-03 04:55:28 -0300

    feat: get all providers from api

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -47,6 +47,7 @@ export const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -64,6 +65,8 @@ export const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
